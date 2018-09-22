55a2851a2a6360bb89200adb2c3f21cdf5cd99a8
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 17:07:56 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan8/pecahan/IF310117110Latihan8Pecahan.java
@@ -0,0 +1,25 @@
+
+package pkgif.pkg3.pkg10117110.latihan8.pecahan;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Bilangan desimal
+ */
+public class IF310117110Latihan8Pecahan {
+
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+
+    }
+    
+}
